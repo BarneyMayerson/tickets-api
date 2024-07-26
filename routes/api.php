8c2f54c8bf8965211bca021ebcb5c52c2b4bfd04
Barney Mayerson <mr.barney.mayerson@gmail.com>
2024-07-26 09:51:55 +0300

    04 How to Version Your API (vi)

--- routes/api.php
+++ routes/api.php
@@ -8,9 +8,8 @@ use Illuminate\Support\Facades\Route;
 Route::post("/login", [AuthController::class, "login"]);
 Route::post("/register", [AuthController::class, "register"]);
 
-Route::get("/tickets", function () {
-    return Ticket::all();
-});
+Route::prefix("v1")->group(base_path("routes/api_v1.php"));
+Route::prefix("v2")->group(base_path("routes/api_v2.php"));
 
 Route::get("/user", function (Request $request) {
     return $request->user();
