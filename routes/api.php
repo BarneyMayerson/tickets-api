810a17c85059775795d4f07c3ad094f713637178
Barney Mayerson <mr.barney.mayerson@gmail.com>
2024-07-26 14:52:27 +0300

    06 Revoking Authentication Tokens

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::post("/login", [AuthController::class, "login"]);
 Route::post("/register", [AuthController::class, "register"]);
+Route::middleware("auth:sanctum")->post("/logout", [
+    AuthController::class,
+    "logout",
+]);
 
 Route::prefix("v1")->group(base_path("routes/api_v1.php"));
 Route::prefix("v2")->group(base_path("routes/api_v2.php"));
